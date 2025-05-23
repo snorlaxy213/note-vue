9e4bcfac8c07f19a4c2f4c3d7109586aaebdc502
snorlax <twoez4vino@foxmail.com>
2025-05-24 05:54:25 +0800

    fix: fold-list的中多个fold情况下header显示重复
    更改fold-list的中多个fold情况下header显示重复的BUG

---
+++ src/components/MakedownShow.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <mavon-editor
+        :boxShadow="false"
+        :defaultOpen="'preview'"
+        :editable="false"
+        :previewBackground="background"
+        :subfield="false"
+        :toolbarsFlag="false"
+        :value="mkValue"
+        style="font-size: 20px"
+    ></mavon-editor>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "markdownShow",
+  props: ["mkValue", "background"],
+};
+</script>
+
+<style scoped></style>
